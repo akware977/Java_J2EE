a0e062f1d114be0dca7b77ab71e3deb645050c15
Akshay <Akshay@DESKTOP-SHSDCAP>
2019-11-30 17:24:26 +0530

    @RequestParam mapping to accept values directly in assign variables

--- SpringMVC/src/main/java/com/spring/AddController.java
+++ SpringMVC/src/main/java/com/spring/AddController.java
@@ -6,6 +6,7 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.service.AddService;
@@ -16,10 +17,11 @@ public class AddController {
 	Logger log = Logger.getLogger(AddController.class);
 	
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest req, HttpServletResponse res) {
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j, 
+				HttpServletRequest req, HttpServletResponse res) {
 		log.info(".....Ureka....");
-		int i = Integer.parseInt(req.getParameter("t1"));
-		int j = Integer.parseInt(req.getParameter("t2"));
+		/*int i = Integer.parseInt(req.getParameter("t1"));
+		int j = Integer.parseInt(req.getParameter("t2"));*/
 		
 		AddService as = new AddService();		
 		int k = as.add(i, j);
